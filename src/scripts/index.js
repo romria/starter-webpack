20a3f45fd36617979f85aa965ccf14d7404d57d5
Roman Riabokon <ryabokon.roman@gmail.com>
2023-07-07 21:26:09 +0300

    some updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import '../fonts/Inter/inter.scss';
+import '../fonts/Roboto/roboto.scss';
 import '../styles/reset.scss';
 import '../styles/index.scss';
 import '../styles/styles.scss';
 
-document.getElementsByClassName('hello')[0].innerHTML = 'Landing page starter is running!';
+document.getElementsByClassName('hello')[0].innerHTML = 'Landing page starter is running';
